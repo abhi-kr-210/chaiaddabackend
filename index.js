5c7169344078b9f4a343ddaf10fe3d5a46b7e610
abhi-kr-210 <abhishekk1.ug21.ec@nitp.ac.in>
2025-01-29 18:56:32 +0530

    data added

--- index.js
+++ index.js
@@ -3,14 +3,22 @@ require('dotenv').config();
 const express=require('express');
 const app=express();
 const port=process.env.PORT||5000;
-app.use(express.json()); 
-
+// app.use(express.json()); 
+const data={
+    "my name":"abhishek",
+    "my age":22,
+    "my country":"india",
+    "my city":"delhi"
+}
 app.get('/',(req,res)=>{
     res.send({"my name":"abhishek"});
 });
 app.get('/login',(req,res)=>{
     res.send('<h1 >Hi! welocme to Abhi_Academy</h1>')
 });
+app.get('/data',(req,res)=>{
+    res.json(data);
+})
 app.listen(port,()=>{
     console.log(`server is started at port ${port}`);
 });
